b7d2216881ae73f1a2de75909a87f7d85d0387c1
s3786643 <s3786643@student.rmit.edu.au>
2021-05-02 20:42:05 +1000

    Add route table code

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-    tags = {
-      "Name" = "Assignment 2 VPC"
-    }
+  tags = {
+    "Name" = "Assignment 2 VPC"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -11,5 +11,18 @@ resource "aws_internet_gateway" "igw" {
 
   tags = {
     Name = "main"
+  }
+}
+
+resource "aws_default_route_table" "main" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Assignment 2 Default Table"
   }
 }
