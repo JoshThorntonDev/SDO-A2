6bfff4df77fa601638a14d5692dc52f9088015f9
s3786643 <s3786643@student.rmit.edu.au>
2021-05-02 20:38:44 +1000

    Add a missing character

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,15 +1,15 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+    cidr_block = "10.0.0.0/16"
 
-  tags = {
-    "Name" = "Assignment 2 VPC"
-  }
+    tags = {
+      "Name" = "Assignment 2 VPC"
+    }
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "A2 Internet Gateway"
+    Name = "main"
   }
 }
