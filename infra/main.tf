d2156605404f15cb0a568f0e331a24866454528e
s3786643 <s3786643@student.rmit.edu.au>
2021-05-08 17:49:31 +1000

    Create a load balancer target group

--- infra/main.tf
+++ infra/main.tf
@@ -3,3 +3,9 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "aws_lb_target_group" "a2_lb_target_group" {
+  name     = "a2-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
