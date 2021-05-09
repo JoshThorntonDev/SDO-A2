e9aa12a718a13a3946cde2c38fb897946d85d3c8
s3786643 <s3786643@student.rmit.edu.au>
2021-05-09 17:28:18 +1000

    Create security group for db

--- infra/db.tf
+++ infra/db.tf
@@ -1,3 +1,29 @@
 resource "aws_db_subnet_group" "main" {
   subnet_ids = [aws_subnet.data_az1.id, aws_subnet.data_az2.id, aws_subnet.data_az3.id]
 }
+
+resource "aws_security_group" "db" {
+  description = "Allow MongoDB traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "postgres from vpc"
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "Allow MongoDB"
+  }
+
+}
