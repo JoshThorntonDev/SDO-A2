181936edba98f96405c1895031162b60f0944999
s3786643 <s3786643@student.rmit.edu.au>
2021-05-09 17:57:16 +1000

    Update db master_username because "admin" is reserved

--- infra/db.tf
+++ infra/db.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "db" {
 resource "aws_docdb_cluster" "a2-docdb" {
   cluster_identifier     = "docdb-cluster"
   availability_zones     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  master_username        = "admin"
+  master_username        = "dbuser"
   master_password        = "SDOAssignment2"
   vpc_security_group_ids = [aws_security_group.db.id]
   db_subnet_group_name   = aws_db_subnet_group.main.name
