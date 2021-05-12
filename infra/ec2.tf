870f7cfc0a455bfe9dfa2dc165685e06adb34665
s3786643 <s3786643@student.rmit.edu.au>
2021-05-12 15:53:21 +1000

    Change ingress to be from port 80 to port 80

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "allow_http_ssh" {
   ingress {
     description = "http from internet"
     from_port   = 80
-    to_port     = 5000
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
