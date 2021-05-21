a5b377fedef75d9e886e2ae736c92f3de19241de
s3786643 <s3786643@student.rmit.edu.au>
2021-05-21 11:19:33 +1000

    Use variables rather than heaps of shell commands

--- ansible/scripts/run-ansible.sh
+++ ansible/scripts/run-ansible.sh
@@ -7,7 +7,10 @@ echo "  hosts:" >> ansible/inventory.yml
 
 echo "    "$(cd infra && terraform output -raw public_ip) >> ansible/inventory.yml
 
+USERNAME=$(cd infra && terraform output -raw db_username)
+PASSWORD=$(cd infra && terraform output -raw db_pw)
+DNS_NAME=$(cd infra && terraform output -raw db_dns_name)
 
 
 echo "  vars:" >> ansible/inventory.yml
-echo "    db_url: mongodb://"$(cd infra && terraform output -raw db_username)":"$(cd infra && terraform output -raw db_pw)"@"$(cd infra && terraform output -raw db_dns_name) >> ansible/inventory.yml
+echo "    db_url: mongodb://$USERNAME:$PASSWORD@$DNS_NAME" >> ansible/inventory.yml
